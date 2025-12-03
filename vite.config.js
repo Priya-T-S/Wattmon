0a7bfc3ab31d4e51e19b6bdb8c88de3f4fc78e6a
Priya-T-S <priyats224@gmail.com>
2025-12-03 21:07:46 +0530

    Updated login redirect and paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Wattmon/",
   plugins: [react()],
+  base: "/Wattmon/",   // THIS IS MANDATORY
+  build: {
+    outDir: "dist",
+  },
 });
